bcff60c6274779e25f02113d17907654e60577a1
Keunhak Lim <limkeunhak@gmail.com>
2020-09-21 23:20:47 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const Express = require('express');
+const server = new Express();
+
+server.use('/', (req, res) => {
+	res.send("Hello World!");
+});
+
+server.listen(3000, () => {
+	console.log("server is started on port 3000");
+});
